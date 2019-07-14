efec94ca66cedfcb6496488a5cbc5b943d4aaf4d
oksana-d <oksana.yegorova99@gmail.com>
2019-07-14 19:34:23 +0300

    Updated the page of the conference participants

--- src/application/views/SocialNetworks.php
+++ src/application/views/SocialNetworks.php
@@ -1,8 +1,3 @@
-<div class="row">
-    <div class="col">
-        <a href="/participants" class="float-right">All members (<?= $countUser['total'] ?>)</a>
-    </div>
-</div>
 <div class="row share">
     <a href="https://www.facebook.com/sharer/sharer.php?u=<?=$config['link']?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
         <img src="../src/img/facebook.jpeg" alt="facebook">
@@ -10,4 +5,9 @@
     <a href="http://twitter.com/share?&url=<?=$config['link']?>&text=<?=$config['text']?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
         <img src="../src/img/twitter.jpeg" alt="twitter">
     </a>
+</div>
+<div class="row">
+    <div class="col">
+        <a href="/participants" class="float-right">All members (<?= $countUser['total'] ?>)</a>
+    </div>
 </div>
