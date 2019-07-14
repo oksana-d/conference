4d794a71b891cd5fd751ac0427e11b9385c65d2b
oksana-d <oksana.yegorova99@gmail.com>
2019-07-14 18:51:35 +0300

    Updated display of the list of conference participants

--- src/application/views/ParticipantsView.php
+++ src/application/views/ParticipantsView.php
@@ -5,33 +5,40 @@
         </div>
     </div>
     <?php if (isset($users)): ?>
+        <div class="row">
+            <div class="col">
+                <div class="table-responsive text-nowrap">
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Photo</th>
+                            <th>Name</th>
+                            <th>Report subject</th>
+                            <th>Email</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($users as $key => $user): ?>
+                            <tr>
+                                <td scope="row"><?= ++$key ?></td>
+                                <td>
+                                    <img class="img rounded-circle profile-img" src="<?= !empty($user['photo']) ? $user['photo'] : '../src/img/no-image.png' ?>" alt="user_photo">
+                                </td>
+                                <td><?= $user['firstname'] ?> <?= $user['lastname'] ?></td>
+                                <td><?= $user['reportSubject'] ?></td>
+                                <td><a href="mailto:<?= $user['email'] ?>"><?= $user['email'] ?></a></td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
     <div class="row">
         <div class="col">
-            <table class="table table-striped table-responsive">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Photo</th>
-                        <th>Name</th>
-                        <th>Report subject</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($users as $key => $user): ?>
-                        <tr>
-                            <th scope="row"><?= ++$key ?></th>
-                            <td>
-                                <img class="img rounded-circle profile-img" src="<?= !empty($user['photo']) ? $user['photo'] : 'img/no-image.png' ?>" alt="user_photo">
-                            </td>
-                            <td><?= $user['firstname'] ?> <?= $user['lastname'] ?></td>
-                            <td><?= $user['reportSubject'] ?></td>
-                            <td><a href="mailto:<?= $user['email'] ?>"><?= $user['email'] ?></a></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+            <a href="/" class="btn btn-success float-right">Back to homepage</a>
         </div>
     </div>
-    <?php endif; ?>
 </div>
